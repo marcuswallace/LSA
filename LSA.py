ed7efef88a214645da8cfe9504744fae2d03a866
Marcus Wallace <marcuswallace@bandwidth.com>
2018-10-03 23:56:25 -0400

    fixing build method

--- LSA.py
+++ LSA.py
@@ -24,13 +24,10 @@ class LSA(object):
                 self.count +=1
 
     def build(self):
-        self.keys = [k for k in self.word_dict.keys() if len(self.word_dict[k]) > 1]
+        self.keys = [k for k in self.word_dict.keys() if len(self.word_dict[k]) > 1] #might have to change this
         self.keys.sort()
-        self.A = zeros([len(self.keys), self.count])
-        for i, k in enumerate(self.keys):
-            for d in self.word_dict[k]:
-                self.A[i,d-1] += 1
-
+        self.A = zeros([len(self.keys)])
+        print self.A
     def transform(self):
         word_doc_count = sum(self.A, axis=0)
         doc_word_count = sum(asarray(self.A > 0), axis=1)
--- LSA_Test_Suite.py
+++ LSA_Test_Suite.py
@@ -35,8 +35,26 @@ def test_parse_and_build():
     mylsa.parse(test_string)
     mylsa.parse(test_string_two)
     mylsa.build()
-    print mylsa.A
+    mylsa.printMatrix()
 
-test_parse_and_build()
+#test_parse_and_build()
 #test_parse()
 #test_parse_with_restrictions()
+titles =[
+    "The Neatest Little Guide to Stock Market Investing",
+    "Investing For Dummies, 4th Edition",
+    "The Little Book of Common Sense Investing: The Only Way to Guarantee Your Fair Share of Stock Market Returns",
+    "The Little Book of Value Investing",
+    "Value Investing: From Graham to Buffett and Beyond",
+    "Rich Dad's Guide to Investing: What the Rich Invest in, That the Poor and the Middle Class Do Not!",
+    "Investing in Real Estate, 5th Edition",
+    "Stock Investing For Dummies",
+    "Rich Dad's Advisors: The ABC's of Real Estate Investing: The Secrets of Finding Hidden Profits Most Investors Miss"
+]
+stopwords = ['and','edition','for','in','little','of','the','to']
+ignorechars = ''',:'!'''
+mylsa = LSA(stopwords, ignorechars)
+for t in titles:
+    mylsa.parse(t)
+mylsa.build()
+#mylsa.printMatrix()
